135a40ad2c2527c1359669a611d8e9c8e69c983a
stardz <bn_benali@esi.dz>
2015-04-20 16:37:06 +0100

    nedjme 2

--- app/src/main/java/com/devmobile/cs2/esi_lib/LoginActivity.java
+++ app/src/main/java/com/devmobile/cs2/esi_lib/LoginActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.widget.TabHost;
 import android.widget.TabHost.OnTabChangeListener;
 
-import com.tabs_swipe.R;
 
 public class LoginActivity extends FragmentActivity implements OnTabChangeListener, OnPageChangeListener {
 
--- app/src/main/java/com/devmobile/cs2/esi_lib/SignInFragment.java
+++ app/src/main/java/com/devmobile/cs2/esi_lib/SignInFragment.java
@@ -7,8 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-import com.tabs_swipe.R;
 
 public class SignInFragment extends Fragment {
 
@@ -17,7 +17,7 @@ public class SignInFragment extends Fragment {
 			Bundle savedInstanceState) {
 
 		View rootView = inflater.inflate(R.layout.fragment_sign_in, container, false);
-		
+
 		return rootView;
 	}
 }
